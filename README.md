2ea9053446a720164ac6526729c16f5609a193e2
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-05-08 23:21:11 -0500

    updated read me and java

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Java-Quiz
+
+## Description
+
+We were tasked with creating a multiple choice coding quiz. Using javascript the html and css were updated dynamically. Once the use clicks the start button the quiz the first question will be displayed, and the timer will begin to decrease. The questions are displyed one at a time. Once the user chooses an answer they will be presented with the next question. They will also be notified whether their answer was correct or incorrect. Incorrect answers will subtract 10s off the timer. Once the user answers the final question the time remaining will be displayed as thier final score. The user will then enter their initials/name and save that score to a high score list. The user can then either retry the quiz and a new score the the highscore list, or they can choose to clear the existing highscores. 
+
+## Licence
+
+License: MIT License
+
+Copyright (c) [2021] [steve siegel]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,37 @@
+// variables for page elements
+// time and score
 let timeEl = document.querySelector("p.time");
-let secondsLeft = 100;
+let secondsLeft = 75;
+let scoreEl = document.querySelector("#score");
 
-
+// sections
+// section intro
 const introEl = document.querySelector("#intro");
+
+// section questions
+//question section
 const questionsEl = document.querySelector("#questions");
-let questionEl = document.querySelector("question");
-
+//where question goes
+let questionEl = document.querySelector("#question");
+// how many questions they have answered
 let questionCount = 0;
+// div yaynay
+const rightwrong = document.querySelector("#yaynay");
 
-const rightwrongEl = document.querySelector("#rightwrong");
+// section final
+const finalEl = document.querySelector("#final");
+// user initials
+let initialsInput = document.querySelector("#initials");
+
+// section highscores
+const highscoresEl = document.querySelector("#highscores");
+// ordered list
+let scoreListEl = document.querySelector("#score-list");
+// array of scores
+let scoreList = [];
 
 // buttons
-
+// start
 const startBtn = document.querySelector("#start");
 // answer button class
 const ansBtn = document.querySelectorAll("button.ansBtn")
@@ -23,8 +43,14 @@ const ans2Btn = document.querySelector("#answer2");
 const ans3Btn = document.querySelector("#answer3");
 // answer4
 const ans4Btn = document.querySelector("#answer4");
-
-
+// submit-score
+const submitScrBtn = document.querySelector("#submit-score");
+// goback
+const goBackBtn = document.querySelector("#goback");
+// clearscores
+const clearScrBtn = document.querySelector("#clearscores");
+// view-scores
+const viewScrBtn = document.querySelector("#view-scores");
 // questions
 
 const questions = [
@@ -45,19 +71,15 @@ const questions = [
 
 // functions
 
-// Functions
-
 // timer
 function setTime() {
-    let timerInterval = setInterval(function () {
+    let timerinterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = `Time:${secondsLeft}s`;
 
         if (secondsLeft === 0 || questionCount === questions.length) {
             clearInterval(timerInterval);
-            questionsEl.style.display = "none";
-            finalEl.style.display = "block";
-            scoreEl.textContent = secondsLeft;
+            questionsEl.style.display = "none";            
         }
     }, 1000);
 }
@@ -65,14 +87,13 @@ function setTime() {
 // start quiz with timer and set up questions
 function startQuiz() {
     introEl.style.display = "none";
-    questionsEl.style.display = "block";
+    questionsEl.styele.display ="block";
     questionCount = 0;
 
     setTime();
     setQuestion(questionCount);
 }
 
-// function to set question; takes in a count and displays the next question/answers
 function setQuestion(id) {
     if (id < questions.length) {
         questionEl.textContent = questions[id].question;
@@ -83,117 +104,28 @@ function setQuestion(id) {
     }
 }
 
-// function to check answer and then move to next question
 function checkAnswer(event) {
     event.preventDefault();
 
-    // show section for yaynay and append message
-    yaynayEl.style.display = "block";
+    rightwrong.style.display = "block";
     let p = document.createElement("p");
-    yaynayEl.appendChild(p);
+    rightwrong.appendChild(p);
 
-    // time out after 1 second
     setTimeout(function () {
         p.style.display = 'none';
     }, 1000);
 
-    // answer checker
     if (questions[questionCount].correctAnswer === event.target.value) {
         p.textContent = "Correct!";
     } else if (questions[questionCount].correctAnswer !== event.target.value) {
         secondsLeft = secondsLeft - 10;
-        p.textContent = "Wrong!";
+        p.textContent = "Incorrect!";
     }
 
-    // increment so the questions index is increased
     if (questionCount < questions.length) {
         questionCount++;
     }
-    // call setQuestion to bring in next question when any ansBtn is clicked
     setQuestion(questionCount);
 }
 
-function addScore(event) {
-    event.preventDefault();
-
-    finalEl.style.display = "none";
-    highscoresEl.style.display = "block";
-
-    let init = initialsInput.value.toUpperCase();
-    scoreList.push({ initials: init, score: secondsLeft });
-
-    // sort scores
-    scoreList = scoreList.sort((a, b) => {
-        if (a.score < b.score) {
-          return 1;
-        } else {
-          return -1;
-        }
-      });
-    
-    scoreListEl.innerHTML="";
-    for (let i = 0; i < scoreList.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
-        scoreListEl.append(li);
-    }
-
-    // Add to local storage
-    storeScores();
-    displayScores();
-}
-
-function storeScores() {
-    localStorage.setItem("scoreList", JSON.stringify(scoreList));
-}
-
-function displayScores() {
-    // Get stored scores from localStorage
-    // Parsing the JSON string to an object
-    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
-
-    // If scores were retrieved from localStorage, update the scorelist array to it
-    if (storedScoreList !== null) {
-        scoreList = storedScoreList;
-    }
-}
-
-// clear scores
-function clearScores() {
-    localStorage.clear();
-    scoreListEl.innerHTML="";
-}
-
-// EventListeners
-// Start timer and display first question when click start quiz
 startBtn.addEventListener("click", startQuiz);
-
-// Check answers loop
-ansBtn.forEach(item => {
-    item.addEventListener('click', checkAnswer);
-});
-
-// Add score
-submitScrBtn.addEventListener("click", addScore);
-
-// Go Back Button
-goBackBtn.addEventListener("click", function () {
-    highscoresEl.style.display = "none";
-    introEl.style.display = "block";
-    secondsLeft = 75;
-    timeEl.textContent = `Time:${secondsLeft}s`;
-});
-
-// Clear the scores
-clearScrBtn.addEventListener("click", clearScores);
-
-// View/Hide High Scores Button
-viewScrBtn.addEventListener("click", function () {
-    if (highscoresEl.style.display === "none") {
-        highscoresEl.style.display = "block";
-    } else if (highscoresEl.style.display === "block") {
-        highscoresEl.style.display = "none";
-    } else {
-        return alert("No scores to show.");
-    }
-});
